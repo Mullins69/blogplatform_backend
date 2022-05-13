45b7ac314cbced32aa4575ecd0b41806b514f7fa
Mullins <mullinsatheem@gmail.com>
2022-05-13 19:30:05 +0200

    fixed post

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,28 +1,32 @@
 <template>
   <div class="container">
     <div class="row">
-        <div class="col-12">
-           <!-- Button trigger modal -->
+      <div class="col-12">
+        <!-- Button trigger modal -->
 
-
-          <h3>Filter</h3>
-          <select
-            v-model="selected"
-            class="form-select"
-            aria-label="Default select example"
-          >
-
-            <option selected value="">Display All</option>
-            <option value="sport">Sport</option>
-            <option value="food">Food</option>
-            <option value="politics">Politics</option>
-          </select>
-        </div>
+        <h3>Filter</h3>
+        <select
+          v-model="selected"
+          class="form-select"
+          aria-label="Default select example"
+        >
+          <option selected value="">Display All</option>
+          <option value="sport">Sport</option>
+          <option value="food">Food</option>
+          <option value="politics">Politics</option>
+        </select>
       </div>
+    </div>
     <div class="row" v-if="blogs">
-    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal" v-if="loggedIn == true">
-      Add Post
-</button>
+      <button
+        type="button"
+        class="btn btn-primary"
+        data-bs-toggle="modal"
+        data-bs-target="#exampleModal"
+        v-if="loggedIn == true"
+      >
+        Add Post
+      </button>
       <div class="col">
         <div class="post" v-for="blog of filterBlogs" :key="blog._id">
           <div class="blog_post" v-for="data of blog.post" :key="data._id">
@@ -40,7 +44,11 @@
               </p>
             </div> -->
           </div>
-          <router-link class="readmore" :to="{ name: 'readmore', params: { id: blog._id } }" >Read More ...</router-link>
+          <router-link
+            class="readmore"
+            :to="{ name: 'readmore', params: { id: blog._id } }"
+            >Read More ...</router-link
+          >
         </div>
       </div>
     </div>
@@ -52,48 +60,65 @@
     </div>
   </div>
   <Contact />
- 
-<!-- Modal -->
-<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Add Post</h5>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div><form
-    class="container"
-    @submit.prevent="addPost"
-  >
-      <div class="modal-body">
-          <input type="text" v-model="title" placeholder="title" required >
-         <select
-            v-model="category"
-            class="form-select"
-            aria-label="Default select example"
-            required
-          >
 
-            <option selected value="food">Food</option>
-            <option value="sport">Sport</option>
-            <option value="politics">Politics</option>
-          </select>
-           <input type="text" placeholder="img link" v-model=" thisimg" required>
-            <input type="text" placeholder="details" v-model="details" required>
+  <!-- Modal -->
+  <div
+    class="modal fade"
+    id="exampleModal"
+    tabindex="-1"
+    aria-labelledby="exampleModalLabel"
+    aria-hidden="true"
+  >
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLabel">Add Post</h5>
+          <button
+            type="button"
+            class="btn-close"
+            data-bs-dismiss="modal"
+            aria-label="Close"
+          ></button>
+        </div>
+        <form class="container" @submit.prevent="addPost">
+          <div class="modal-body">
+            <input type="text" v-model="title" placeholder="title" required />
+            <select
+              v-model="category"
+              class="form-select"
+              aria-label="Default select example"
+              required
+            >
+              <option selected value="food">Food</option>
+              <option value="sport">Sport</option>
+              <option value="politics">Politics</option>
+            </select>
+            <input type="text" placeholder="img link" v-model="img" required />
+            <input
+              type="text"
+              placeholder="details"
+              v-model="details"
+              required
+            />
+          </div>
+
+          <div class="modal-footer">
+            <button
+              type="button"
+              class="btn btn-secondary"
+              data-bs-dismiss="modal"
+            >
+              Close
+            </button>
+            <button type="submit" class="btn btn-primary">Save changes</button>
+          </div>
+        </form>
       </div>
-     
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-        <button type="submit" class="btn btn-primary">Save changes</button>
-      </div>
-       </form>
     </div>
   </div>
-</div>
 </template>
 
 <script>
-import authHeader from "../services/auth-header";
-
 import Loader from "../components/Loader.vue";
 import Contact from "@/components/Contact.vue";
 export default {
@@ -106,24 +131,29 @@ export default {
     return {
       blogs: null,
       selected: "",
-      loggedIn:  this.$store.state.auth.status.loggedIn,
+      loggedIn: this.$store.state.auth.status.loggedIn,
       title: "",
-      img:"",
-      details:"",
-      category:""
+      img: "",
+      details: "",
+      category: "",
     };
   },
-  methods:{
-    addPost(){
-        fetch("https://blogplatapi.herokuapp.com/posts", {
+  methods: {
+    addPost() {
+      fetch("https://blogplatapi.herokuapp.com/posts", {
         method: "POST",
         body: JSON.stringify({
           title: this.title,
-          img:this.img,
-          details:this.details,
-          category:this.category
+          img: this.img,
+          details: this.details,
+          category: this.category,
         }),
-        headers: authHeader(),
+
+        headers: {
+          "Content-type": "application/json; charset=UTF-8",
+
+          Authorization: `Bearer ${JSON.parse(localStorage.getItem("user"))}`,
+        },
       })
         .then((response) => response.json())
         .then((json) => {
@@ -133,9 +163,8 @@ export default {
         .catch((err) => {
           alert(err);
         });
-    }
-  }
-  ,
+    },
+  },
   mounted() {
     fetch("https://blogplatapi.herokuapp.com/posts", {
       method: "GET",
@@ -151,36 +180,32 @@ export default {
         alert(err);
         console.log(err);
       });
-  },computed: {
+  },
+  computed: {
     filterBlogs: function () {
-      let filtered = this.blogs
-      if (this.selected == '') {
-          filtered = filtered.filter((blogs) => {
-           return blogs.category.match(this.selected) ;
-          
+      let filtered = this.blogs;
+      if (this.selected == "") {
+        filtered = filtered.filter((blogs) => {
+          return blogs.category.match(this.selected);
         });
         // if(this.search){
         //   filtered = filtered.filter((blogs) =>{
         //     return blogs.title.match(this.search)
         //   })
         // }
-        return filtered
+        return filtered;
       }
       if (this.selected) {
         filtered = filtered.filter((blogs) => {
-           return blogs.category.match(this.selected) ;
-          
+          return blogs.category.match(this.selected);
         });
         // if(this.search){
         //   filtered = filtered.filter((blogs) =>{
         //     return blogs.title.match(this.search)
         //   })
         // }
-        return filtered
-        
+        return filtered;
       }
-  
-      
     },
   },
 };
@@ -201,7 +226,6 @@ export default {
   margin: 50px;
   box-shadow: 2px 2px 8px #e4e4e4;
   text-align: left;
- 
 }
 
 img {
@@ -222,10 +246,10 @@ img {
   font-weight: 500;
   font-size: 15px;
   line-height: 30px;
-  color:#6c757d!important;
+  color: #6c757d !important;
 }
 
-.readmore{
+.readmore {
   text-decoration: none;
   border-bottom: 3px solid #000;
   font-family: "Inter";
@@ -235,10 +259,8 @@ img {
   color: #000000;
 }
 
-.readmore:hover{
+.readmore:hover {
   color: red;
   border-bottom: 3px solid red;
 }
-
-
 </style>
